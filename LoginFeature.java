ef76a5bf0c13dab0b442169a40e4d529c54888ce
yasminmss1311 <ywsminmss@gmail.com>
2025-11-10 21:51:24 -0300

    Add files via upload

---
+++ LoginFeature.java
@@ -0,0 +1,8 @@
+public class LoginFeature {
+    public static void login(){
+        System.out.println("Login realizado com sucesso!");
+    }
+    public static void main(String[] args) {
+        login();
+    }
+}
